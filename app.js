fd644f82e45c073b4b09790138699591ff1db5f7
prafful-PC\prafful <prafful.s@gloobdecor.com>
2016-09-28 14:44:34 +0530

    First

---
+++ app.js
@@ -0,0 +1,30 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: b1b01b79-d78d-4218-9466-b8b191f0100c,
+    appPassword: qwerASD4
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
