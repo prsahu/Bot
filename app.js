5af897f601eaabcd9ab0c24ca67b7d4897679b10
prafful-PC\prafful <prafful.s@gloobdecor.com>
2016-09-28 19:56:27 +0530

    Forgot to load the sheet

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ function setAuth() {
   }
 
   function readthevalues(){
+ doc.getInfo(function(err, info) {
+      console.log('Loaded doc: '+info.title+' by '+info.author.email);
+      sheet = info.worksheets[0];
+      console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+      
+    });
+
       sheet.getCells({
 'worksheet_id':0,
 'min-row':1,
