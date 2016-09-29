0ca0e75a1655e82f78a86164211dae5d16d9de1b
prsahu <prafful.sahu1@gmail.com>
2016-09-29 12:40:25 +0530

    Trying the new menu features

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bot
-A simple example of a bot using Microsoft Bot Framework which can be deployed to Heroku
+A simple example of a bot using Microsoft Bot Framework which can be deployed to Heroku.
+It fetches data from a google spreadsheets to populate data and show menu options
--- app.js
+++ app.js
@@ -52,17 +52,66 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+var google = require('googleapis');
 //=========================================================
 // Bots Dialogs
 //=========================================================
-
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-    for (var i = 0; i < dishes.length; i++) {
-        session.send(dishes[i]);
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.dialog('/Welcome', [
+  function (session) {
+    session.send("Namaste! Welcome to Indian Kitchen");
+    session.send("What can we do for you today?");
+    session.Prompts.choice(session,"Choose One","Place an order|View Menu|Leave Review|(quit)");
+  },
+  function(session,results){
+    if (results.response && results.response.entity != '(quit)') {
+            // Launch demo dialog
+            session.beginDialog('/' + results.response.entity);
+    } else {
+            // Exit the menu
+            session.endDialog();
     }
-});
+  },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/Welcome');
+    }
+]);
 
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
+bot.dialog('/Place an order',[
+  function(session){
+
+  }
+]);
+
+bot.dialog('/View Menu',[
+  function(session){
+    session.send("You can send the user a list of cards as multiple attachments in a single message...");
+
+        var msg = new builder.Message(session)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Space Needle")
+                    .subtitle("The Space Needle is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ]),
+                new builder.HeroCard(session)
+                    .title("Pikes Place Market")
+                    .subtitle("Pike Place Market is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
+                    ])
+            ]);
+        session.endDialog(msg);
+  }
+]);
+
+bot.dialog('/Leave Review',[
+  function(session){
+    builder.Prompts.text(session, "What company do you work for?");
+  },
+  function(session,results){
+    session.send("Here is your review"+results.response);
+  }
+]);
