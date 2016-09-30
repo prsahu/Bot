01002b7c8b9e5cfc4bb9db0ac6f692744b0cf4ce
prafful-PC\prafful <prafful.s@gloobdecor.com>
2016-09-30 17:27:16 +0530

    Added mongo db save order

--- app.js
+++ app.js
@@ -113,6 +113,52 @@ var bot = new builder.UniversalBot(connector, { persistConversationData: true })
 server.post('/api/messages', connector.listen());
 
 var google = require('googleapis');
+
+//=========================================================
+//MongoDB
+//=========================================================
+var dataBase;
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Connection URL 
+var url = process.env.MongoDBURL;
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server"); 
+  dataBase = db;
+});
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection 
+  var collection = db.collection('orders');
+  // Insert some documents 
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+
+function SaveTheOrder(session){
+  var collection = dataBase.collection('orders');
+  
+  var order={};
+
+  
+   db.collection('orders').insertOne( {
+      "details" : {
+         "name" : session.userData.name,
+         "phoneNumber" : session.userData.phoneNumber         
+      },
+      "order":sess.conversationData
+   });
+}
 //=========================================================
 // Bots Dialogs
 //=========================================================
@@ -225,6 +271,7 @@ bot.dialog('/Order Confirmation',[
       
       session.send("Thanks you for your order again %s",session.userData.name);
       //console.log("End");
+      SaveTheOrder(session);
       session.endConversation("");
     }else{
       session.replaceDialog('/User Information');
@@ -234,8 +281,9 @@ bot.dialog('/Order Confirmation',[
 
 bot.dialog('/User Information',[
   function(session){
-    builder.Prompts.confirm(session, "Are you a returning foodie?. Answer yes or no now.");
-  },
+    //builder.Prompts.confirm(session, "Are you a returning foodie?. Answer yes or no now.");
+    builder.Prompts.text(session,"What is your name?");
+  }/*,
   function (session, results) {
     if(results.response){
       session.send("I am afraid I have a bad memory");
@@ -245,7 +293,7 @@ bot.dialog('/User Information',[
     }else{
       builder.Prompts.text(session,"What is your name?");
     }
-  },
+  }*/,
   function(session,results){
     session.userData.name= results.response;
     var temp = "Hi "+results.response+"! Can I have your phone number also??";
@@ -254,7 +302,8 @@ bot.dialog('/User Information',[
   function (session,results){
     session.userData.phoneNumber = results.response;
     session.userData.contactDetails = true;
-    session.endConversation("");
+    session.replaceDialog('/Order Confirmation');
+    //session.endConversation("");
   }
 ]);
 
